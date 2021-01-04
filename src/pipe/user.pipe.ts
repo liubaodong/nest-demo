338a38d0c7b7352940d4c15eb454484511f0e363
liubaodong <2458825436@qq.com>
2021-01-04 23:45:09 +0800

    管道验证

--- src/pipe/user.pipe.ts
+++ src/pipe/user.pipe.ts
@@ -1,9 +1,13 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
+import { ObjectSchema } from '@hapi/joi';
 @Injectable()
 export class UserPipe implements PipeTransform {
+  constructor(private schema: ObjectSchema) {}
   transform(value: any, metadata: ArgumentMetadata) {
-    console.log('user 管道')
+    const { error } = this.schema.validate(value);
+    if (error) return { success: false };
+    value.id = '666';
+    console.log('user 管道');
     return value;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,22 @@
 import { Controller, UsePipes, Get, Query } from '@nestjs/common';
 import { UserPipe } from 'src/pipe/user.pipe';
+import * as Joi from '@hapi/joi';
+
+let userSchema = Joi.object().keys({
+  name: Joi.string().required(),
+  age: Joi.number().integer().min(6).max(66).required(),
+});
 
 @Controller('user')
 export class UserController {
   @Get()
   index() {
-    return '用户页面'
+    return '用户页面';
   }
   @Get('pipe')
-  @UsePipes(new UserPipe())
+  @UsePipes(new UserPipe(userSchema))
   pipe(@Query() info) {
-    console.log('info', info)
-    return info
+    console.log('info', info);
+    return info;
   }
 }
